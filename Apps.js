9d38c07e263b6a23e27811808fcb85d48ff11299
AshwiniNakerakanti123 <126052000+AshwiniNakerakanti123@users.noreply.github.com>
2023-03-04 00:17:07 +0530

    doc: not completed fully

--- Apps.js
+++ Apps.js
@@ -1,23 +1,23 @@
-var buttonTrans = document.querySelectorAll("button");
+var buttonTranslate = document.querySelector("button-translation");
 var inputTxt = document.querySelector("Input-Text");
 var outputTxt = document.querySelector("Output-Text");
-var MorseURL = "https://api.funtranslations.com/translate/morse.json";
+var ServerURL = "https://api.funtranslations.com/translate/morse.json"
 
-function getTransURL(text){
-    return MorseURL + "?" + "text=" + text ;
+function getTranslationURL(text){
+    return ServerURL + "?" + "text=" + text 
 }
 function errorHandler(error){
     console.log("error occured ..", error );
-    alert("Somthing wrong check Once Or Try after some time");
+    alert("Somthing wrong check Once Or Try after some time")
 }
 function clickHandler(){
-    var inputText = inputTxt.value ;
-     fetch(getTransURL(inputTxt))
+    var inputText = inputTxt.value;
+     fetch(getTranslationURL(inputText))
      .then(response => response.json())
      .then(json =>{
         var translatedText = json.contents.translated;
         outputTxt.innerHTML = translatedText;
      })
      .catch(errorHandler)
-}
-buttonTrans.addEventListener("click" clickHandler);
+};
+buttonTrans.addEventListener("click" ,clickHandler);
